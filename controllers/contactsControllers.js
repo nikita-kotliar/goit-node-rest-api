c847825004e33c92bad3cbbb87f8fe30c0bb6c1d
nikita-kotliar <knr200710@gmail.com>
2024-05-10 18:45:03 +0300

    new

---
+++ controllers/contactsControllers.js
@@ -0,0 +1,88 @@
+import HttpError from "../helpers/HttpError.js";
+import {
+  createContactSchema,
+  updateContactSchema,
+} from "../schemas/contactsSchemas.js";
+import Contact from "../models/Contact.js";
+
+
+export const getAllContacts = async (req, res) => {
+  try {
+    const contacts = await Contact.find();
+    return res.status(200).json(contacts);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getOneContact = async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const contact = await contact.findById(id);
+    if (!contact) {
+      throw HttpError(404);
+    }
+    res.status(200).json(contact);
+  } catch (error) {
+    const status = error.status || 500;
+    res.status(status).json({ message: error.message });
+  }
+};
+
+export const createContact = async (req, res, next) => {
+  try {
+    const { name, email, phone } = req.body;
+    const { error } = createContactSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400);
+    }
+    const newContact = new Contact({ name, email, phone });
+    const savedContact = await newContact.save();
+    res.status(201).json(savedContact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteContact = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+
+    const removedContact = await Contact.findByIdAndDelete(id);
+    if (!removedContact) {
+      throw HttpError(404);
+    }
+    res.status(200).json(removedContact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateContact = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const { name, email, phone } = req.body;
+    const { error } = updateContactSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, "Body must have at least one field");
+    }
+    const updatedContact = await Contact.findByIdAndUpdate(
+      id,
+      {
+        name,
+        email,
+        phone,
+      },
+
+      { new: true }
+    );
+    if (!updatedContact) {
+      throw HttpError(404);
+    }
+    res.status(200).json(updatedContact);
+  } catch (error) {
+    next(error);
+  }
+};
+
