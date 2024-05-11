41e5bee06c69a4455632522cf094508c8a484d27
nikita-kotliar <knr200710@gmail.com>
2024-05-11 18:18:34 +0300

    new branch update

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,85 @@
-import contactsService from "../services/contactsServices.js";
+import HttpError from "../helpers/HttpError.js";
+import {
+  createContactSchema,
+  updateContactSchema,
+} from "../schemas/contactsSchemas.js";
+import сontact from "../models/Contact.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res) => {
+  try {
+    const contacts = await сontact.find();
+    return res.status(200).json(contacts);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res, next) => {
+  const { id } = req.params;
 
-export const deleteContact = (req, res) => {};
+  try {
+    const contact = await contact.findById(id);
+    if (!contact) {
+      throw HttpError(404);
+    }
+    res.status(200).json(contact);
+  } catch (error) {
+    const status = error.status || 500;
+    res.status(status).json({ message: error.message });
+  }
+};
 
-export const createContact = (req, res) => {};
+export const createContact = async (req, res, next) => {
+  try {
+    const { name, email, phone } = req.body;
+    const { error } = createContactSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400);
+    }
+    const newContact = new сontact({ name, email, phone });
+    const savedContact = await newContact.save();
+    res.status(201).json(savedContact);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const updateContact = (req, res) => {};
+export const deleteContact = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const removedContact = await сontact.findByIdAndDelete(id);
+    if (!removedContact) {
+      throw HttpError(404);
+    }
+    res.status(200).json(removedContact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateContact = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const { name, email, phone } = req.body;
+    const { error } = updateContactSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, "Body must have at least one field");
+    }
+    const updatedContact = await сontact.findByIdAndUpdate(
+      id,
+      {
+        name,
+        email,
+        phone,
+      },
+
+      { new: true }
+    );
+    if (!updatedContact) {
+      throw HttpError(404);
+    }
+    res.status(200).json(updatedContact);
+  } catch (error) {
+    next(error);
+  }
+};
